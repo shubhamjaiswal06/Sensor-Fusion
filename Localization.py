9e85a2803b245ef02ab346f9045a3d3990c5f234
badave007 <badave007@gmail.com>
2020-12-27 19:22:34 +0100

    CU: Variance of center position and height added

--- Localization.py
+++ Localization.py
@@ -24,18 +24,18 @@ def Gradient_decent_cost_function(actual_height,f_length,QR_global_pose,robot_es
     
     estimated_height_QR = actual_height * f_length /(np.sqrt(Square_both)) 
     estimated_Cx = f_length * np.tan(np.arctan(second_term/first_term) - psyi)
-    Estimated = [[estimated_height_QR], [estimated_Cx]]
+    Estimated = np.array([[estimated_height_QR], [estimated_Cx]])
     
     Jacobean_Mat = jacobean_calculations(actual_height, f_length,first_term,second_term,Square_both,psyi)
     
-    cost = Jacobean_Mat.transpose() * (Measurement - Estimated)
+    Gradient_decent_cost = Jacobean_Mat.transpose() * (Measurement - Estimated)
     
-    return cost
+    return Gradient_decent_cost
 
 def jacobean_calculations(actual_height, f_length,first_term,second_term,Square_both,psyi):
     
     ##This function creates the jacobean matrix for calculations ##
-    # For the current measurement model, 2 X 3 jacobean will be created
+    # For the current measurement model, 2 rows and 3 colomns jacobean will be created
     
     Jacobean_Mat[0,0] = actual_height * f_length * (first_term) / pow(Square_both,3/2)
     Jacobean_Mat[0,1] = actual_height * f_length * (second_term) / pow(Square_both,3/2)
@@ -52,7 +52,7 @@ prev_QR = None
 QR_codes = set()
 file_path = "E:/ashutosh/sensorfusionstuff/shubham project/dataset3/data/task5"
 file_name = "camera_localization_task5.csv"
-f_length = 524 #focal length in pixels
+
 #data = csv.reader(file_path+file_name,delimiter=',')
 data = np.genfromtxt(file_path+'/'+file_name,delimiter=',')
 QR_codes.update(data[:,1])
@@ -69,34 +69,48 @@ for row in data:
         print(count)
         break
         
-sorted_data = np.sort(data,axis=0)
-sorted_data[:,6] = sorted_data[:,6]+6.7
-avg_distance = []
-var_distance = []
+sorted_data = sorted(data,key=lambda x: x[1])
+#sorted_data[:,6] = sorted_data[:,6]+6.7
+avg_measured_height = []
+var_measured_height = []
+avg_center_pos = []
+var_center_pos = []
 P_x = 35
 P_y = 46
 Sigma = 75
+Measurement = np.array([[0],[2]])           # considered initial measurement 
+Jacobean_Mat = np.array([[0,0,0],[0,0,0]])  # Initial Jacobean Matrix
+actual_height = 11.5                        # Actual height in cm
 
-Jacobean_Mat = np.array([[0,0,0],[0,0,0]])
+f_length = 524                              #focal length in pixels
+step_size = 0.02                            # Gradient Decent Step_size
+
+
 for row in sorted_data:
     curr_QR = row[1]
     
     if prev_QR == None:
         prev_QR = curr_QR
-        distance = []
+        measured_height = []
+        center_pos = []
     if prev_QR == curr_QR:
-        distance.append(row[6])
-        
+        measured_height.append(row[5])
+        center_pos.append(row[2])
     else:
-        avg_distance.append(np.mean(distance))
-        var_distance.append(np.var(distance))
-        print(np.mean(distance))
-        print(np.var(distance))
-        distance.clear()
-        prev_QR = curr_QR
-        distance.append(row[6])
+        avg_measured_height.append(np.mean(measured_height))
+        var_measured_height.append(np.var(measured_height))
         
-avg_distance.append(np.mean(distance))
-var_distance.append(np.var(distance))
+        var_center_pos.append(np.var(center_pos))
+        print(np.mean(measured_height))
+        print(np.var(measured_height))
+        measured_height.clear()
+        center_pos.clear()
+        prev_QR = curr_QR
+        measured_height.append(row[5])
+        center_pos.append(row[2])
+        
+avg_measured_height.append(np.mean(measured_height))
+var_measured_height.append(np.var(measured_height))
 
+var_center_pos.append(np.var(center_pos))
 
