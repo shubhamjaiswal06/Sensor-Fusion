eeaf145f88e114d2a1f09b1beed8874c2624f5b8
badave007 <badave007@gmail.com>
2020-12-27 18:04:52 +0100

    Added jacobean calculation function

--- Localization.py
+++ Localization.py
@@ -14,6 +14,33 @@ import pandas as pd
 
 # for i in a:
 #     print(i)
+
+
+def cost_function(Jacobean_Mat,measurement,estimated):
+    print("Inside cost function")
+    cost = Jacobean_Mat.transpose() * (measurement - estimated)
+    return cost
+
+def jacobean_calculations(actual_height, f_length,QR_global_pose,robot_estimated_pose,psyi):
+    
+    ##This function creates the jacobean matrix for calculations ##
+    # For the current measurement model, 2 X 3 jacobean will be created
+    
+    first_term = QR_global_pose[0] - robot_estimated_pose[0]
+    second_term = QR_global_pose[0] - robot_estimated_pose[0]
+    Square_both = first_term**2 + second_term**2
+    
+    Jacobean_Mat[0,0] = actual_height * f_length * (first_term) / pow(Square_both,3/2)
+    Jacobean_Mat[0,1] = actual_height * f_length * (second_term) / pow(Square_both,3/2)
+    Jacobean_Mat[0,2] = 0
+    Jacobean_Mat[1,0] = f_length  * pow(np.arccos(np.arctan(second_term/first_term) - psyi),2) * (second_term / Square_both)
+    Jacobean_Mat[1,1] = -f_length * pow(np.arccos(np.arctan(second_term/first_term) - psyi),2) * (first_term / Square_both)
+    Jacobean_Mat[1,2] = -f_length * pow(np.arccos(np.arctan(second_term/first_term) - psyi),2) 
+    #Jacobean_Mat = 
+    
+    return Jacobean_Mat
+    
+    
 prev_time = None
 prev_QR = None
 QR_codes = set()
@@ -40,6 +67,11 @@ sorted_data = np.sort(data,axis=0)
 sorted_data[:,6] = sorted_data[:,6]+6.7
 avg_distance = []
 var_distance = []
+P_x = 35
+P_y = 46
+Sigma = 75
+
+Jacobean_Mat = np.array([[0,0,0],[0,0,0]])
 for row in sorted_data:
     curr_QR = row[1]
     
@@ -60,3 +92,5 @@ for row in sorted_data:
         
 avg_distance.append(np.mean(distance))
 var_distance.append(np.var(distance))
+
+
